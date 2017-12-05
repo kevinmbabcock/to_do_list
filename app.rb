6759f5c3b469cb9c42b2a085600aa85fb5a0db3e
Guest User <Guest@epicodus-9.local>
2017-12-04 16:17:21 -0800

    add front end views

--- app.rb
+++ app.rb
@@ -2,6 +2,41 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload("lib/**/*.rb")
 require('./lib/task')
+require('./lib/list')
 require('pg')
 
 DB = PG.connect({:dbname => "to_do"})
+
+get ("/") do
+  erb(:index)
+end
+
+get("/lists/new") do
+  erb(:list_form)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:success)
+end
+
+get("/lists") do
+  @lists = List.all()
+  erb(:lists)
+end
+
+get("/lists/:id") do
+  @list = List.find(params[:id].to_i)
+  erb(:list)
+end
+
+post("/tasks") do
+  description = params[:description]
+  list_id = params[:list_id].to_i
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id})
+  @task.save
+  erb(:tasks)
+end
